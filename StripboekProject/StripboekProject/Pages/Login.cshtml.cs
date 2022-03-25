1c894f02a6877037c2bd7998668a063052c45e83
Stefan366 <stefan.wulff@student.nhlstenden.com>
2022-03-25 12:15:12 +0100

    Register/ Login Functions

--- StripboekProject/StripboekProject/Pages/Login.cshtml.cs
+++ StripboekProject/StripboekProject/Pages/Login.cshtml.cs
@@ -1,9 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace StripboekProject.Pages;
 
 public class Login : PageModel
 {
+    [BindProperty]
+    public Viewmodel.Login login { get; set; }
     public void OnGet()
     {
 
--- StripboekProject/StripboekProject/Pages/Register.cshtml.cs
+++ StripboekProject/StripboekProject/Pages/Register.cshtml.cs
@@ -1,11 +1,28 @@
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace StripboekProject.Pages;
 
 public class Register : PageModel
 {
+    [BindProperty]
+    public Viewmodel.Register register { get; set; }
     public void OnGet()
     {
 
+    }
+    
+    public void OnPost()
+    {
+        var name = "";
+        var password = "";
+        var id = "";
+        var role = "";
+
+        if (ModelState.IsValid)
+        {
+            // var db = (Database).open("Table")
+            
+        }
     }
 }

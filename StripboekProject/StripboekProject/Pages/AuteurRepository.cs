1adb79a478c36e6c5cc0d6089afa0eeb97da6a47
Dennis <dennisvm2283@gmail.com>
2022-03-28 14:14:54 +0200

    test 2e table
    Table veranderen kijken of het iets veranderd

--- StripboekProject/StripboekProject/Pages/AuteurRepository.cs
+++ StripboekProject/StripboekProject/Pages/AuteurRepository.cs
@@ -4,7 +4,7 @@ using MySql.Data.MySqlClient;
 
 namespace StripboekProject.Pages;
 
-public class CollectieRepository
+public class AuteurRepository
 {
     private IDbConnection Connect()
     {
@@ -16,14 +16,14 @@ public class CollectieRepository
     }
 /* Connection aanroepen en met dapper Query uitvoeren
     Hij pakt het stripboek wat het meest overeenkomt met de titel van wat je invoert*/
-    public List<Stripboek> Search(string search)
+    public List<Auteur> Search(string search)
     {
         using var connection = Connect();
-        var  collectie= connection.Query<Stripboek>("SELECT * FROM Collectie_naam WHERE Collectie_naam LIKE @search", new
+        var  auteurs= connection.Query<Auteur>("SELECT * FROM StripboekenWebApp.Auteur WHERE Auteur_naam LIKE @search", new
         {
             search = "%" + search + "%"
         });
-        return collectie.ToList();
+        return auteurs.ToList();
         
             
         

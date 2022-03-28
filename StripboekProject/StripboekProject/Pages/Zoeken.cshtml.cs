de204f48ffe7410e265ce2a3ada2af36b7aaf4a4
Dennis <dennisvm2283@gmail.com>
2022-03-28 18:10:52 +0200

    Zoeken pagina stripboek laten zien
    In het zoeken tab kan je nu de stripboeken zien vanuit de database (nog wel maar 1 exemplaar)

--- StripboekProject/StripboekProject/Pages/Zoeken.cshtml.cs
+++ StripboekProject/StripboekProject/Pages/Zoeken.cshtml.cs
@@ -1,9 +1,20 @@
+using System.Data;
+using Dapper;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using MySql.Data.MySqlClient;
 
 namespace StripboekProject.Pages;
 
 public class Zoeken : PageModel
 {
+
+    public IEnumerable<Stripboek> Stripboeken
+    {
+        get
+        {
+            return new Zoekenlist().Get();
+        }
+    }
     public void OnGet()
     {
 

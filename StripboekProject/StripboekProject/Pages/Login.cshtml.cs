bfcefd1d5c02c830782dc126041e54f271101bf5
Stefan366 <91754308+Stefan366@users.noreply.github.com>
2022-03-30 20:57:05 +0200

    login and register (login heeft een issue met het redirecten van de page)

--- StripboekProject/StripboekProject/Pages/Login.cshtml.cs
+++ StripboekProject/StripboekProject/Pages/Login.cshtml.cs
@@ -1,14 +1,37 @@
+using System.Data;
+using Dapper;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using MySql.Data.MySqlClient;
+
+
 
 namespace StripboekProject.Pages;
 
-public class Login : PageModel
+public partial class Login : PageModel
 {
+    private IDbConnection Connect() 
+    {
+        return new MySqlConnection(
+            "Server=127.0.0.1,;Port = 3306;" +
+            "Database=StripboekenWebApp;" +
+            "Uid=root;Pwd=;"
+        );
+    }
+    
     [BindProperty]
-    public Viewmodel.Login login { get; set; }
+    public MadeAccount profile { get; set; }
     public void OnGet()
     {
 
+    }
+
+    public void OnPost()
+    {
+        if (ModelState.IsValid)
+        {
+            // als input wordt geacepteerd wordt er gecheckt in de database en doorverwezen als dit succesvol is
+            new StripboekRepository().SignIn(profile);
+        }
     }
 }
--- StripboekProject/StripboekProject/Pages/StripboekRepository.cs
+++ StripboekProject/StripboekProject/Pages/StripboekRepository.cs
@@ -1,19 +1,23 @@
 using System.Data;
+using System.Runtime.CompilerServices;
 using Dapper;
-using Microsoft.AspNetCore.Components.Web;
-using Microsoft.AspNetCore.Connections.Features;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 using MySql.Data.MySqlClient;
+
 
 namespace StripboekProject.Pages;
 
-public class StripboekRepository
+public class StripboekRepository : PageModel
 {
+
     private IDbConnection Connect()
     {
         return new MySqlConnection(
             "Server=127.0.0.1;Port=3306;" +
             "Database=StripboekenWebApp;" +
-            "Uid=dennis;Pwd=password;"
+            "Uid=root;Pwd=;"
         );
     }
 /* Connection aanroepen en met dapper Query uitvoeren
@@ -27,12 +31,29 @@ public class StripboekRepository
         });
         return stripboeken.ToList();
     }
-
-    public void Add(Stripboek stripboek) 
+    // het toevegen van een account in register voor de account table in de database
+    public void Add(Account account)
     {
-        using var connection = Connect();
-        int numRowsEffected = connection.Execute
-        ("INSERT INTO Stripboek (StripId, Titel, Aantal_blz, Serie, Serienummer, Uitgave, Dikte_mm, Lengte_mm, Breedte_mm, NSFW, Digitaal, Verified) VALUES(@StripId, @Titel, @Aantal_blz, @Serie, @Serienummer, @Uitgave, @Dikte_mm, @Lengte_mm, @Breedte_mm, @NSFW, @Digitaal, @Verified)"
-            , new {StripId = stripboek.StripId, Titel = stripboek.Titel, Aantal_blz = stripboek.Aantal_blz, Serie = stripboek.Serie, Serienummer = stripboek.Serienummer, Uitgave = stripboek.Uitgave, Dikte_mm = stripboek.Dikte_mm, Lengte_mm = stripboek.Lengte_mm, Breedte_mm = stripboek.Breedte_mm, NSFW = stripboek.NSFW, Digitaal = stripboek.Digitaal, Verified = stripboek.Verified});
+        using var secondConnection = Connect();
+        string sql =
+            @"INSERT INTO account (Naam, Wachtwoord, AccountId) 
+            VALUES (@Naam, @Wachtwoord, @AccountId)";
+        int RowsInserted = secondConnection.Execute(sql, account);
+        // als dit gemergged is in main check insert into hierboven voor hun datasource (database tabelnamen zijn bij mij in lowercase en kan misschien betekenen dat ze niet direct koppelen zonder correctie op hun hoofdletters) 
+    }
+    // het checken van de account database om de input te valideren en vervolgens naar home door te verwijzen   
+    public IActionResult SignIn(MadeAccount profile)
+    {
+        using var thirdConnection = Connect();
+        string select = @"SELECT * FROM account WHERE Naam = @Naam AND Wachtwoord = @Wachtwoord";
+        object RowsFound = thirdConnection.QuerySingle(select, profile);
+        // hier graag dezelfde check als bij de insert into in Add functie hierboven maar dan bij select * from hier
+        object workaround = null;
+        if (RowsFound != null)
+        {
+            return RedirectToPage("/Login/Home");
+        }
+
+        return null;
     }
 }

842284ffa509d5093ebcae4f419a4412c567bf0f
MatthijsStreekstra <matthijs.streekstra@hotmail.nl>
2022-03-31 11:54:20 +0200

    Merge
    MergeMain

--- StripboekProject/StripboekProject/Pages/Add.cshtml.cs
+++ StripboekProject/StripboekProject/Pages/Add.cshtml.cs
@@ -22,7 +22,7 @@ public class Add : PageModel
         }
 
         Msg = "Nieuw stripboek succesvol toegevoegd! ";
-        new StripboekRepository().Add(NewStripboek);
+        new StripboekRepository().AddStripboek(NewStripboek);
         return RedirectToPage("SearchResult");
     }
 }
